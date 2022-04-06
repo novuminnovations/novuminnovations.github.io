64610f0056fc089296a65f3ce176869459bf583c
zaxiaa <97071782+zaxiaa@users.noreply.github.com>
2022-04-06 06:00:40 -0400

    typer

--- static/js/script.js
+++ static/js/script.js
@@ -39,3 +39,38 @@ function onClick(element) {
       }
   }
 
+  $(function(){
+	$(".typed").typed({
+		strings: ["back-end solutions with Python.", "back-end solutions with Java.", "full-stack solutions with Javascript.", "front-end solutions with HTML.", "front-end solutions with CSS.", "back-end solutions with SQL."],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 10,
+		// time before typing starts
+		startDelay: 1200,
+		// backspacing speed
+		backSpeed: 10,
+		// time before backspacing
+		backDelay: 500,
+		// loop
+		loop: true,
+		// false = infinite
+		loopCount: 5,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+	});
+});
