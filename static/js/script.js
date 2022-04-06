690ec65d28f3a7b54db9d2bbd65f2e19909e0b0d
zaxiaa <97071782+zaxiaa@users.noreply.github.com>
2022-04-06 05:00:45 -0400

    html js

--- static/js/script.js
+++ static/js/script.js
@@ -22,3 +22,19 @@ function onClick(element) {
           x.className = x.className.replace(" w3-show", "");
       }
   }
+
+  $(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".tag");
+  
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
+  
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible");
+      } else {
+        $(tag).removeClass("visible");
+      }
+    }
+  });  
