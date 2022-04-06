6ff5dbd4fb3ffe7ba4db82ea46fccea5a4f6745e
novuminnovations <103052097+novuminnovations@users.noreply.github.com>
2022-04-06 08:35:35 -0400

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,3 @@
-$(document).on("scroll", function() {
-    var pageTop = $(document).scrollTop();
-    var pageBottom = pageTop + $(window).height();
-    var tags = $(".tag");
-  
-    for (var i = 0; i < tags.length; i++) {
-      var tag = tags[i];
-  
-      if ($(tag).position().top < pageBottom) {
-        $(tag).addClass("visible");
-      } else {
-        $(tag).removeClass("visible");
-      }
-    }
-  });  
-
 function onClick(element) {
     document.getElementById("img01").src = element.src;
     document.getElementById("modal01").style.display = "block";
@@ -38,39 +22,3 @@ function onClick(element) {
           x.className = x.className.replace(" w3-show", "");
       }
   }
-
-  $(function(){
-	$(".typed").typed({
-		strings: ["back-end solutions with Python.", "back-end solutions with Java.", "full-stack solutions with Javascript.", "front-end solutions with HTML.", "front-end solutions with CSS.", "back-end solutions with SQL."],
-		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-		stringsElement: null,
-		// typing speed
-		typeSpeed: 10,
-		// time before typing starts
-		startDelay: 1200,
-		// backspacing speed
-		backSpeed: 10,
-		// time before backspacing
-		backDelay: 500,
-		// loop
-		loop: true,
-		// false = infinite
-		loopCount: 5,
-		// show cursor
-		showCursor: false,
-		// character for cursor
-		cursorChar: "|",
-		// attribute to type (null == text)
-		attr: null,
-		// either html or text
-		contentType: 'html',
-		// call when done callback function
-		callback: function() {},
-		// starting callback function before each string
-		preStringTyped: function() {},
-		//callback for every typed string
-		onStringTyped: function() {},
-		// callback for reset
-		resetCallback: function() {}
-	});
-});
